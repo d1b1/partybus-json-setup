209066756139bd4e2b5e7784bf78950e0050aa00
Stephan L. Smith <stephan@meenta.io>
2024-03-26 15:27:49 -0400

    Added in initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Party Bus JSON Builder
+This is a POC for a JSON builder for party bus resources.
+
+## File Structure
+The following is the pre-configuration setup for a `partybus` configuration (JSON) file.
+
+- `src/schema.json` contains the JSON schema (also referred to as 'data schema')
+- `src/uischema.json` contains the UI schema
---
+++ src/App.tsx
@@ -0,0 +1,133 @@
+import { Fragment, useState, useMemo } from 'react';
+import { JsonForms } from '@jsonforms/react';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+import './App.css';
+import schema from './schema.json';
+import uischema from './uischema.json';
+import {
+  materialCells,
+  materialRenderers,
+} from '@jsonforms/material-renderers';
+import RatingControl from './RatingControl';
+import ratingControlTester from './ratingControlTester';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  container: {
+    padding: '1em',
+    width: '100%',
+  },
+  title: {
+    textAlign: 'left',
+    padding: '0.25em',
+  },
+  dataContent: {
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: '0.20em',
+    backgroundColor: '#cecece',
+    marginBottom: '1rem',
+  },
+  resetButton: {
+    display: 'flex',
+    margin: '0 10px 10px 0 !important'
+  },
+  demoform: {
+    margin: 'auto',
+    padding: '1rem',
+  },
+});
+
+const initialData = {
+  name: 'New Agent Configuration',
+  description: '',
+  tools: [
+    { uid: '11111', description: 'Sends an SMS via Twilio.', api_spec: 'https://google.com', permissions: 'read' }
+  ],
+  agents: [
+    { uid: '', name: '', description: '' }
+  ],
+  data: [
+    { uid: '11111', permissions: 'read', description: 'Google Places API' },
+    { uid: '22222', permissions: 'write', description: 'Google Weather API' }
+  ]
+};
+
+const renderers = [
+  ...materialRenderers,
+  //register custom renderers
+  { tester: ratingControlTester, renderer: RatingControl },
+];
+
+const App = () => {
+  const classes = useStyles();
+  let [data, setData] = useState<any>(initialData);
+  const stringifiedData = useMemo(() => JSON.stringify(data, null, 2), [data]);
+
+  const saveData = () => {
+    alert('Save Data to a file!')
+  }
+
+  const clearData = () => {
+    setData({});
+  };
+
+  return (
+    <Fragment>
+      <Grid
+        container
+        justifyContent={'center'}
+        spacing={0}
+        className={classes.container}
+      >
+        <Grid item sm={4}>
+          {/* <Typography variant={'h4'} className={classes.title}>
+            JSON Data
+          </Typography> */}
+
+          <div className={classes.dataContent}>
+            <pre id='boundData'>{stringifiedData}</pre>
+          </div>
+
+          <Button
+            className={classes.resetButton}
+            onClick={clearData}
+            color='primary'
+            variant='contained'
+          >
+            Reset the Form?
+          </Button>
+
+          <Button
+            className={classes.resetButton}
+            onClick={saveData}
+            color='primary'
+            variant='contained'
+          >
+            Save the JSON File
+          </Button>
+
+        </Grid>
+        <Grid item sm={8}>
+          {/* <Typography variant={'h4'} className={classes.title}>
+            Rendered form
+          </Typography> */}
+          <div className={classes.demoform}>
+            <JsonForms
+              schema={schema}
+              uischema={uischema}
+              data={data}
+              renderers={renderers}
+              cells={materialCells}
+              onChange={({ errors, data }) => setData(data)}
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
+};
+
+export default App;
